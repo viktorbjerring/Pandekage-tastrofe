f4c95bc3ee9c7c1191aa61c88a29eeb4b4011d30
Mikkel Kirkegaard <fisk9889@gmail.com>
2020-04-07 13:29:51 +0200

    Update af I2C_master

--- Moduler/Uimplementeret moduler/Protokoller/Software/ProtocolTest/I2C_MASTER.h
+++ Moduler/Uimplementeret moduler/Protokoller/Software/ProtocolTest/I2C_MASTER.h
@@ -12,10 +12,10 @@ typedef enum {
 	I2C_MASTER_PRESCALE_64 = 3,
 } I2C_MASTER_prescale_t;
 
-void I2C_MASTER_sendData(char addr, I2C_commands_t cmd);		//Write kommando
-void I2C_MASTER_readData(char addr);							//Read fra slave
-bool I2C_MASTER_checkData();									//Check for new data
-char I2C_MASTER_getData();										//Read the data				
-void I2C_MASTER_init(char bitRate, I2C_MASTER_prescale_t pre);	// 1MHz/(2*bitRate * pre)
+I2C_err_t I2C_MASTER_sendData(char addr, I2C_commands_t cmd);		//Write kommando
+I2C_err_t I2C_MASTER_readData(char addr);							//Read fra slave
+bool I2C_MASTER_checkData();										//Check for new data
+char I2C_MASTER_getData();											//Read the data				
+void I2C_MASTER_init(char bitRate, I2C_MASTER_prescale_t pre);		// 1MHz/(2*bitRate * pre)
 
 #endif

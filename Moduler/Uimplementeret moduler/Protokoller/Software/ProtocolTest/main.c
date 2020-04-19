77174a2b2ff726a247b31319b7a2156890291b9e
drunot <antonvigen@hotmail.com>
2020-04-19 12:05:29 +0200

    Protocoller opdateret

--- Moduler/Uimplementeret moduler/Protokoller/Software/ProtocolTest/main.c
+++ Moduler/Uimplementeret moduler/Protokoller/Software/ProtocolTest/main.c
@@ -15,9 +15,12 @@
 #include "I2C_MASTER.h"
 #include "USART.h"
 
+void wait();
 
 int main(void)
 {
+	DDRB|= 1<<5 ;
+	PORTB = 0 << 5;
 	uint8_t c = 0x00;
 	I2C_SLAVE_init();
 	I2C_MASTER_init(255,I2C_MASTER_PRESCALE_64);
@@ -33,12 +36,20 @@ int main(void)
 		I2C_MASTER_sendData(0x70,BEGIN_COOCKING);
 		_delay_ms(500);
 		I2C_MASTER_sendData(0x70,MAKE_PANCAKE);
-		_delay_ms(500);
 		sendChar(I2C_SLAVE_readFirst());
 		sendChar(I2C_SLAVE_readFirst());
 		sendChar(I2C_SLAVE_readFirst());*/
 		I2C_SLAVE_sendData(c);
-		I2C_MASTER_readData(0x70);
+		I2C_err_t ret = I2C_MASTER_readData(0x70);
+		while(ret != I2C_NOT_EXCLUSIVE){
+			if(ret == I2C_READ_FAIL);
+			{
+				sendChar(0xEE);
+			}
+			ret = I2C_MASTER_readData(0x70);
+			PORTB = 1 << 5;
+		}
+		PORTB = 0 << 5;
 		if(c == 255)
 		{
 			c = 0;
@@ -47,10 +58,18 @@ int main(void)
 		{
 			c++;
 		}
-		while(!I2C_MASTER_checkData()){}
+		wait();
 		sendChar(I2C_MASTER_getData());
 		
 		
     }
 }
 
+#pragma GCC optimize 0
+void wait()
+{
+	while(!I2C_MASTER_checkData())
+	{
+	}
+}
+#pragma GCC reset_options

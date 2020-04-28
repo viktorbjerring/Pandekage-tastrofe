9150f34cb1ce02a7d747372921bc525798acb14e
Morten <mortenhaahrkristensen@gmail.com>
2020-04-28 17:21:46 +0200

    Mit hjerte gør ondt

---
+++ Moduler/Uimplementeret moduler/RPI_server/flaskServer_v5.2/OrderHandling.py
@@ -0,0 +1,23 @@
+from communication import userSpaceCMD
+import os
+import time
+import threading
+
+class OrderHandling:
+    def __init__(self, orderOverviewObj):
+        self.__orderOverviewObj = orderOverviewObj
+        t1 = threading.Thread(target=self.checkIfPancakeIsDone, daemon=True).start() # Daemon means shut down thread immediately if we try to stop the proces
+        
+    def newOrder(self):
+        userSpaceCMD("MAKE_PANCAKE") # Make new pancake
+        print("Told system to make a new pancake.")
+        return
+    def checkIfPancakeIsDone(self):
+        while True:
+            time.sleep(10) # Do once every 10 seconds
+            with open('/dev/pan', 'r') as systemfile: 
+                contents = systemfile.read()
+                print("Checked if pancake was done - result was:")
+                print(contents)
+            if(contents[0] == "1"):
+                self.__orderOverviewObj.oldestPancakeDone()

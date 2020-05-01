0319276b1f3585eeeae9f3fa7ed7f7f1709766cd
drunot <antonvigen@hotmail.com>
2020-05-01 12:46:15 +0200

    Diagram

--- Moduler/Uimplementeret moduler/PandeMekanik/Modultest/Varmelegeme_steprespons/varmelegeme_maalinger_norm_script.m
+++ Moduler/Uimplementeret moduler/PandeMekanik/Modultest/Varmelegeme_steprespons/varmelegeme_maalinger_norm_script.m
@@ -40,14 +40,14 @@ RC_Time = interp1(rmmissing(Temp60uq), rmmissing(T.Tid60(Temp60uqIndex)),  RC60)
 
 % Finder T_s.
 T_s = T_s_end-T_s_begin;
-hold on;
-plot(T_s_begin, steadyStatePct10,  'o');
-plot(T_s_end, steadyStatePct90,  'o');
-plot(RC_Time, RC60,  'o');
-title('Steprespons for varmelegeme i sekunder');
-xlabel('Tid [sek]');
-ylabel('Temperatur [°C]');
-grid on;
+%hold on;
+%plot(T_s_begin, steadyStatePct10,  'o');
+%plot(T_s_end, steadyStatePct90,  'o');
+%plot(RC_Time, RC60,  'o');
+%title('Steprespons for varmelegeme i sekunder');
+%xlabel('Tid [sek]');
+%ylabel('Temperatur [°C]');
+%grid on;
 
 %% I minutter
 
@@ -85,24 +85,56 @@ steadyState80 = RC80/(1-exp(-1));
 
 %%
 syms ss;
-G60 = ((steadyState60-steadyStateMinReal60)*(1/(RC_Time-100)))/((1/(RC_Time-100))+ss);
-wn=0.0048;
-z = 0.93;
-G60Own = (wn^2)/((ss^2)+(2*z*wn*ss)+wn^2);
+s = tf('s');
+G60 = ((steadyState60)*(1/(RC_Time-100)))/((1/(RC_Time-100))+ss);
+
+wn50=0.0049;
+z50 = 0.9;
+G50Own = (wn50^2)/((ss^2)+(2*z50*wn50*ss)+wn50^2)*(200-T.Temp50(1))/0.5+T.Temp50(1)/0.5;
+G50Own_Bode = (wn50^2)/((s^2)+(2*z50*wn50*s)+wn50^2)*(200-T.Temp50(1))/0.5+T.Temp50(1)/0.5;
+
+G50OwnOL = 1/(1/G50Own-1);
+G50OwnOL_Bode = 1/(1/G50Own_Bode-1);
+
+wn60=0.0043;
+z60 = 0.85;
+G60Own = (wn60^2)/((ss^2)+(2*z60*wn60*ss)+wn60^2)*(175-T.Temp60(1))/0.6+T.Temp60(1)/0.6;
+G60Own_Bode = (wn60^2)/((s^2)+(2*z60*wn60*s)+wn60^2)*(175-T.Temp60(1))/0.6+T.Temp60(1)/0.6;
+
+G60OwnOL = 1/(1/G60Own-1);
+G60OwnOL_Bode = 1/(1/G60Own_Bode-1);
+
+wn70=0.004;
+z70 = 0.86;
+G70Own = (wn70^2)/((ss^2)+(2*z70*wn70*ss)+wn70^2)*(172-T.Temp70(1))/0.7+T.Temp70(1)/0.7;
+G70Own_Bode = (wn70^2)/((s^2)+(2*z70*wn70*s)+wn70^2)*(172-T.Temp70(1))/0.7+T.Temp70(1)/0.7;
+
+G70OwnOL = 1/(1/G70Own-1);
+G70OwnOL_Bode = 1/(1/G70Own_Bode-1);
+
 figure('name','Steprespons'); clf;
-ezplot((ilaplace(G60*1/ss))/(steadyState60-steadyStateMinReal60),[0,1500]);
+%fplot((ilaplace(G60*1/ss))/(steadyState60-steadyStateMinReal60),[0,1500]);
+fplot(ilaplace(G50Own*(0.5/ss)),[0,1800]);
 hold on;
-ezplot(ilaplace(G60Own*1/ss),[0,1500]);
-plot((T.Tid50),(T.Temp60-steadyStateMinReal50)/(steadyState50-steadyStateMinReal50));
-plot((T.Tid60),(T.Temp60-steadyStateMinReal60)/(steadyState60-steadyStateMinReal60));
-plot((T.Tid70),(T.Temp70-steadyStateMinReal70)/(steadyState70-steadyStateMinReal70));
+fplot(ilaplace(G60Own*(0.6/ss)),[0,1800]);
+fplot(ilaplace(G70Own*0.7/ss),[0,1800]);
+plot((T.Tid50),(T.Temp50));
+plot((T.Tid60),(T.Temp60));
+plot((T.Tid70),(T.Temp70));
 hold off;
-legend('1. ordens approx step', '2. ordens approx step', 'Step ved 50% duty', 'Step ved 60% duty', 'Step ved 70% duty', 'Location', 'southeast');
-ylim([0 1])
+legend('epprox 50', 'epprox 60', 'epprox 70', '50', '60', '70', 'Location', 'southeast');
+%ylim([0 1])
+
+figure('name','Bodeplot'); clf;
+bode(G50OwnOL_Bode);
+hold on;
+bode(G60OwnOL_Bode);
+bode(G70OwnOL_Bode);
+legend('50', '60', '70', 'Location', 'southeast');
 
 %% Test plot af fit ved 100 % og frekvens plot
 figure('name','Steprespons'); clf;
-ezplot(ilaplace(G60Own*1/ss),[0,1500]);
+fplot(ilaplace(G60Own*1/ss),[0,1500]);
 hold on;
 plot((T.Tid100),(T.Temp100-steadyStateMinReal100)/280);
 
@@ -114,34 +146,29 @@ plot([50,60,70],[steadyState50,steadyState60,steadyState70]);
 ts = 10*60;
 
 kp = 1/0.01-1;
-syms ss;
-s = tf('s');
-
-G60Own = (wn^2)/((s^2)+(2*z*wn*s)+wn^2);
 
 figure('name','Bodeplot'); clf;
-bode(G60Own);
+bode(G60OwnOL_Bode);
 
 wBW = 0.0067503;
-curMag = bode(G60Own, wBW);
+curMag = bode(G60OwnOL_Bode, wBW);
 curMagdB = 20*log10(curMag);
 
 propMagdB = -1*curMagdB;
 
 propMag = 10^(propMagdB/20);
 
-Gr_1 = G60Own * propMag;
+Gr_1 = G60OwnOL_Bode * propMag;
 
 figure('name','Bodeplot'); clf;
 margin(Gr_1);
 H_1 = feedback(Gr_1,1);
 
 figure('name', 'Stepresponse'); clf;
-step(H_1);
+step(Gr_1/(1+Gr_1));
 hold on;
-syms ss;
-G60OwnSyms = (wn^2)/((ss^2)+(2*z*wn*ss)+wn^2);
-ezplot(ilaplace(G60OwnSyms*1/ss),[0,1500]);
+step(G60OwnOL_Bode/(G60OwnOL_Bode+1));
+%fplot(ilaplace((G60Own*0.6/ss))/(175-T.Temp60(1)),[0,1500]);
 hold off;
 %%
 beta=(1-sin(45/360*2*pi))/(1+sin(45/360*2*pi));
@@ -159,13 +186,13 @@ margin(Gr_2);
 figure('name','Step'); clf;
 step(Gr_2/(1+Gr_2));
 hold on;
-
+step(Gr_2);
+hold off;
 %%
 T2 = 1.1/wBW;
 a = 10^((40-7.3)/20);
 GLag=(s+1/T2)/(s+1/(a*T2));
 Gr_3 = Gr_2 * GLag;
-G60Own_syms = (wn^2)/((ss^2)+(2*z*wn*ss)+wn^2);
 
 figure('name','Bodeplot for Lead'); clf;
 bode(GLag)
@@ -176,7 +203,22 @@ margin(Gr_3);
 figure('name','Step'); clf;
 step(Gr_3/(1+Gr_3));
 hold on;
-fplot(ilaplace(G60Own_syms*1/ss),[0,1500],'-r');
-plot(T.Tid100,(T.Temp100-steadyStateMinReal100)/(200-steadyStateMinReal100));
+step(G60Own_Bode/293);
+hold off;
 legend('reguleret system', 'ureguleret system');
 hold off;
+
+%%
+wBW = 0.0067503;
+curMag = bode(G60Own_Bode, wBW);
+curMagdB = 20*log10(curMag);
+
+propMagdB = -1*curMagdB;
+
+Gp = 10^(propMagdB/20);
+
+figure('name','step');
+step((G60Own_Bode*Gp)/(1+G60Own_Bode*Gp));
+figure('name','step');
+step(G60Own_Bode);
+

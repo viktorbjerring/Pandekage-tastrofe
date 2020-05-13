f2ce5d855cfb005a823928cdeadb94d18e1874d6
drunot <antonvigen@hotmail.com>
2020-05-13 16:05:47 +0200

    EMC

--- EMC/Udregning.m
+++ EMC/Udregning.m
@@ -22,8 +22,13 @@ legend('l = 0.1 m, h = 0.1 m', ...
        'l = 0.5 m, h = 1 m', ...
        'l = 1 m, h = 1 m', ...
        'Location', 'southeast');
-title('EMK induceret i strømsløjfer af forskellig størrelser (E=1 V/m)');
+title('EMK induceret i strømsløjfer af forskellige størrelser (E=1 V/m)');
 xlabel('Frekvens [Hz]');
 ylabel('Induceret spænding [V]');
-grid('on');
+grid on
+ax = gca;
+ax.XAxis.TickValues = [10^3,10^4,10^5,10^6,10^7,10^8,10^9,10^10];
+ax.YAxis.TickValues = [10^(-7),10^(-6),10^(-5),10^(-4),10^(-3),10^(-2),10^(-1),10^0,10^1];
+ax.MinorGridLineStyle = 'none';
 xlim([10^3,10^10]);
+ylim([10^(-7),10^1]);
